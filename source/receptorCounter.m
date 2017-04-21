01fcf975dad17d7b362da324ee710f10f5e1bd28
Choong Jin Ng <jin8383@gmail.com>
2017-04-21 01:24:14 -0700

    Included writeImage function as part of receptorCounter. Exports map image to 'Results'

--- source/receptorCounter.m
+++ source/receptorCounter.m
@@ -167,11 +167,13 @@ if (strcmp(debug,'all') || strcmp(debug,'map'))
         '/' filename '_map'], 'png');   
 end
 
-
 %% Output
 if (exist(['Results/' filename],'dir') ~= 7)
     mkdir(['Results/' filename]);
 end
+
+%% Saving results    
+writeImage(map, [filename '_map'], ['Results/' filename]);
 
 end
 
@@ -423,3 +425,24 @@ end
 %     end
 % end
 % end
+
+%% writeImage: Writes the image to a specified location as a JPEG file
+%
+% INPUT:
+% img - Image to be saved. Assumes need to convert back to Grayscale and to
+% int8
+% filename - Filename of the Image
+% location - Specified location
+function [] = writeImage(img,filename,location)
+temp_img = img;
+temp_img = mat2gray(temp_img);
+temp_img = im2uint8(temp_img);
+
+if (~isempty(location))
+    img_filename = [location '/' filename];
+else
+    img_filename = filename;
+end
+
+imwrite(temp_img, [img_filename '.jpg']);
+end
