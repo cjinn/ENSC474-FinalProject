468f5d8721279ba17499b4c4473e07cc76464d8f
Choong Jin Ng <jin8383@gmail.com>
2017-04-21 01:15:54 -0700

    Calculates the density using the masked image and assigning colour. Removed getFFTImg from receptorCounter.

--- source/receptorCounter.m
+++ source/receptorCounter.m
@@ -35,7 +35,7 @@ k_factor = 16;
 iterations = 100;
 
 %% Initialisation of Debugging variables
-f_version = 'v0.01.004'; % Version of files
+f_version = 'v0.01.005'; % Version of files
 
 %% Initialisation for Debugging
 if (~strcmp(debug,'none') && ...
@@ -54,7 +54,7 @@ temp_img = temp_img(:);
 [~,initial_C] = intensitySegment(img,k_factor);
 index = kmeans(temp_img,k_factor,'Start',initial_C);
 temp_img = reshape(index,size(img));
-temp_img_c{k_factor} = 0;
+temp_img_c{k_factor + 1} = 0;
 
 for ii=1:k_factor
     temp_img_c{ii} = (temp_img == ii);
@@ -135,24 +135,35 @@ if (strcmp(debug,'all') || strcmp(debug,'mask'))
         ['Debug/' f_version '/Mask/' filename '/' filename '_masked'], 'png');
 end
 
-temp_img = masked_img;
+temp_img_c{k_factor + 1} = masked_img;
 
 %% Counting the number of photoreceptors
-photoCount = sum(sum(temp_img));
+photoCount = sum(sum(masked_img));
 
-%% Analysing its FFT of the image
-img_fft = getFFTImg(img);
+%% Mapping density
+density_img = label2rgb(masked_img,'colorcube');
+map = imcomplement(density_img);
 
-if (strcmp(debug,'all') || strcmp(debug,'fft'))
-    if (exist(['Debug/' f_version '/FFT/' filename],'dir') ~= 7)
-        mkdir(['Debug/' f_version '/FFT/' filename]);
+if (strcmp(debug,'all') || strcmp(debug,'map'))
+    if (~exist(['Debug/' f_version '/Map/' filename],'dir'))
+        mkdir(['Debug/' f_version '/Map/' filename]);
     end
     
-    fig = figure('Name',['FFT of ' filename]);
-    mesh(real(img_fft));
-    title(['FFT of ' filename]);
-    saveas(fig, ['Debug/' f_version '/FFT/' filename '/original_fft'], 'png');
+    fig = figure('Name', [filename ' Mapped Image']);
+    subplot(1,3,1);
+    imshow(im2uint8(masked_img));
+    title('Masked Image');
+    subplot(1,3,2);
+    imshow(im2uint8(density_img));
+    title('Applying colour');
+    subplot(1,3,3);
+    imshow(im2uint8(map));
+    title('Complemented colour');
+    saveas(fig, ...
+        ['Debug/' f_version '/Map/' filename ...
+        '/' filename '_map'], 'png');   
 end
+
 
 %% Output
 if (exist(['Results/' filename],'dir') ~= 7)
@@ -222,20 +233,6 @@ for kk=1:k
     C(kk,1) = sum(sum(img.*index(:,:,kk)))/sum(sum(index(:,:,kk)));
 end
 
-end
-
-%% getFFTImg: Gets the image in the frequency domain
-%
-% INPUT:
-% img - Image
-%
-% OUTPUT:
-% img_fft - Image in the Frequency Domain. It has been logged.
-function [img_fft] = getFFTImg(img)
-img_fft = fft2(img);
-img_fft = fftshift(img_fft);
-notzeros = (img_fft ~= 0);
-img_fft(notzeros) = log10(abs(img_fft(notzeros)));
 end
 
 %% mask_image: 
